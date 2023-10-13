dd0ed9ab940e621b2d7708513a2de0f36c50b6af
Christopher-Java <chrlom0905@skola.goteborg.se>
2023-10-13 19:40:34 +0200

    Added a loop in main.java so that a second player can choose character

--- src/Main.java
+++ src/Main.java
@@ -11,42 +11,44 @@ public class Main {
         System.out.println("*   Welcome to Star Wars Character Battle   *");
         System.out.println("*********************************************");
         System.out.println();
-        System.out.println("Choose the character you want to play as : ");
         
-        System.out.println("1. Anakin Skywalker");
-        System.out.println("2. Darth Vader");
-        System.out.println("3. Boba Fett");
-        
-        Scanner scan = new Scanner(System.in);
-        int choice;
-        
-        Character player = null;
-        String playerName = "";
-        
-        do {
-            choice = scan.nextInt();
-            switch (choice) {
-                case 1:
-                    player = new AnakinSkywalker();
-                    playerName = "Anakin Skywalker";
-                    break;
-                
-                case 2:
-                    player = new DarthVader();
-                    playerName = "Darth Vader";
-                    break;
-                
-                case 3:
-                    player = new BobaFett();
-                    playerName = "Boba Fett";
-                    break;
-                
-                default:
-                    System.out.println("Invalid option");
-            }
-        } while (choice < 1 || choice > 3);
-    
-        System.out.println("You chose " + playerName);
-        
+        for (int i = 0; i < 2; i++) {
+            System.out.println("Player " + (i + 1) + "- Choose the character you want to play as :");
+            
+            System.out.println("1. Anakin Skywalker");
+            System.out.println("2. Darth Vader");
+            System.out.println("3. Boba Fett");
+            
+            Scanner scan = new Scanner(System.in);
+            int choice;
+            
+            Character[] player = new Character[2];
+            String[] playerName = new String[2];
+            
+            do {
+                choice = scan.nextInt();
+                switch (choice) {
+                    case 1:
+                        player[i] = new AnakinSkywalker();
+                        playerName[i] = "Anakin Skywalker";
+                        break;
+                    
+                    case 2:
+                        player[i] = new DarthVader();
+                        playerName[i] = "Darth Vader";
+                        break;
+                    
+                    case 3:
+                        player[i] = new BobaFett();
+                        playerName[i] = "Boba Fett";
+                        break;
+                    
+                    default:
+                        System.out.println("Invalid option");
+                }
+            } while (choice < 1 || choice > 3);//Change the condition when more characters are added
+            
+            System.out.println("Player " + (i + 1) + " chose " + playerName[i]);
+        }
     }
 }
